62f82fad544f57117208c7eb030802f2c562eef4
SarahPolfliet <sarah.polfliet1@gmail.com>
2020-11-24 15:07:18 +0100

    Basic implementation of Simple

--- TheFellowshipOfCode.DotNet/TheFellowshipOfCode.DotNet.YourAdventure/MyAdventure.cs
+++ TheFellowshipOfCode.DotNet/TheFellowshipOfCode.DotNet.YourAdventure/MyAdventure.cs
@@ -7,6 +7,7 @@ using HTF2020.Contracts.Enums;
 using HTF2020.Contracts.Models;
 using HTF2020.Contracts.Models.Adventurers;
 using HTF2020.Contracts.Requests;
+using System.Diagnostics;
 
 namespace TheFellowshipOfCode.DotNet.YourAdventure
 {
@@ -43,7 +44,9 @@ namespace TheFellowshipOfCode.DotNet.YourAdventure
 
             Task<Turn> PlayToEnd()
             {
-                return Task.FromResult(request.PossibleActions.Contains(TurnAction.WalkSouth) ? new Turn(TurnAction.WalkSouth) : new Turn(request.PossibleActions[_random.Next(request.PossibleActions.Length)]));
+
+                Dictionary<Location, TileType> TheMap = WriteMap(request.Map);
+                return GetSpecialFeatures(TheMap, request);
             }
 
             Task<Turn> Strategic()
@@ -73,5 +76,88 @@ namespace TheFellowshipOfCode.DotNet.YourAdventure
                 return Task.FromResult(new Turn(request.PossibleActions[_random.Next(request.PossibleActions.Length)]));
             }
         }
+
+        public Dictionary<Location, TileType> WriteMap(Map map)
+        {
+            Dictionary<Location, TileType> tilesWithLocation = new Dictionary<Location, TileType>();
+            for (int x = 0; x < map.Tiles.GetLength(0); x++)
+            {
+                for (int y = 0; y < map.Tiles.GetLength(1); y++)
+                {
+                    tilesWithLocation.Add(new Location(x, y), map.Tiles[x, y].TileType);
+                }
+            }
+            return tilesWithLocation;
+        }
+
+        public Task<Turn> GetSpecialFeatures(Dictionary<Location, TileType> TheMap, PlayTurnRequest request)
+        {
+            Location location = null;
+            foreach (KeyValuePair<Location, TileType> entry in TheMap)
+            {
+                if (entry.Value.Equals(TileType.TreasureChest))
+                {
+                    if (!request.Map.Tiles[entry.Key.X, entry.Key.Y].TreasureChest.IsEmpty)
+                    {
+                        location = entry.Key;
+                        if (request.PossibleActions.Contains(TurnAction.Loot))
+                        {
+                            return Task.FromResult(new Turn(TurnAction.Loot));
+                        }
+                    }
+                }
+                else if (entry.Value.Equals(TileType.Enemy))
+                {
+                    if (!request.Map.Tiles[entry.Key.X, entry.Key.Y].EnemyGroup.IsDead)
+                    {
+                        location = entry.Key;
+                        if (request.PossibleActions.Contains(TurnAction.Attack))
+                        {
+                            return Task.FromResult(new Turn(TurnAction.Attack));
+                        }
+                        if (request.PossibleActions.Contains(TurnAction.Loot))
+                        {
+                            return Task.FromResult(new Turn(TurnAction.Loot));
+                        }
+                    }
+                }
+                else if (entry.Value.Equals(TileType.Finish))
+                {
+                    location = entry.Key;
+                }
+            }
+
+            return ShowPath(location, request);
+        }
+
+        public Task<Turn> ShowPath(Location featureLocation, PlayTurnRequest request)
+        {
+            Location partylocation = request.PartyLocation;
+
+            if (featureLocation != null)
+            {
+                if (featureLocation.X > partylocation.X)
+                {
+                    return Task.FromResult(new Turn(TurnAction.WalkEast));
+                }
+                if (featureLocation.X < partylocation.X)
+                {
+                    return Task.FromResult(new Turn(TurnAction.WalkWest));
+                }
+                if (featureLocation.Y > partylocation.Y)
+                {
+                    return Task.FromResult(new Turn(TurnAction.WalkSouth));
+                }
+
+                if (featureLocation.Y < partylocation.Y)
+                {
+                    return Task.FromResult(new Turn(TurnAction.WalkNorth));
+                }
+            }
+            
+            return Task.FromResult(request.PossibleActions.Contains(TurnAction.WalkSouth)
+                ? new Turn(TurnAction.WalkSouth) : new Turn(request.PossibleActions[_random.Next(request.PossibleActions.Length)]));
+        }
+
     }
 }
